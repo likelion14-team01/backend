d35c022be179abbc35e94f141187ec91610625de
WAcAW9 <wacaw9@gmail.com>
2026-07-06 19:02:52 +0900

    🎉 Start: Inital Commit

---
+++ src/main/java/com/likelion/collab_session/CollabSessionApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.collab_session;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollabSessionApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CollabSessionApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/likelion/collab_session/CollabSessionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.collab_session;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollabSessionApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
